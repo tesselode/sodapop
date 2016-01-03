b41136bc6e1c47a8bb7bcf133a9dc7623a3de608
tesselode <aminnich3@gmail.com>
2016-01-02 22:51:04 -0500

    Center origin function

--- lib.lua
+++ lib.lua
@@ -1,5 +1,12 @@
 local Sprite = {}
 
+function Sprite:centerOrigin()
+  self.ox = self.image:getWidth() / 2
+  self.oy = self.image:getHeight() / 2
+  if self.x then self.x = self.x + self.ox end
+  if self.y then self.y = self.y + self.oy end
+end
+
 function Sprite:draw(x, y)
   assert(x or self.x, 'Sprite has no x position. Set sprite.x or pass an x value into sprite:draw()')
   assert(y or self.y, 'Sprite has no y position. Set sprite.y or pass an y value into sprite:draw()')
--- main.lua
+++ main.lua
@@ -2,15 +2,18 @@ function love.load()
   lib = require 'lib'
 
   testImage = love.graphics.newImage 'mushroom.png'
-  testSprite = lib.newSprite(testImage)
+  testSprite = lib.newSprite(testImage, 50, 100)
 end
 
 function love.keypressed(key)
+  if key == 'return' then
+    testSprite:centerOrigin()
+  end
   if key == 'escape' then
     love.event.quit()
   end
 end
 
 function love.draw()
-  testSprite:draw(50, 100)
+  testSprite:draw()
 end
