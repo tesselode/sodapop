5169d6efee75ee923f88c0e214f1e06c9930874f
Andrew Minnich <aminnich3@gmail.com>
2016-02-20 17:06:02 -0500

    Sprite + nonfunctional sliders

---
+++ demo/main.lua
@@ -0,0 +1,35 @@
+local sodapop = require 'sodapop'
+local suit = require 'suit'
+
+local mushroom = sodapop.newAnimatedSprite(600, 200)
+mushroom:addAnimation('walk', {
+  image = love.graphics.newImage 'mushroom walk.png',
+  frameWidth = 64,
+  frameHeight = 64,
+  frames = {
+    {1, 1, 4, 1, .15}
+  }
+})
+
+local sliders = {
+  rotation = {name = 'Rotation', value = 0, min = 0, max = 2 * math.pi},
+  sx = {name = 'Horizontal scale', value = 1, min = 0, max = 5},
+  sy = {name = 'Vertical scale', value = 1, min = 0, max = 5},
+}
+
+function love.update(dt)
+  suit.layout:reset(100, 100)
+  suit.layout:padding(10, 10)
+
+  for _, slider in pairs(sliders) do
+    suit.Label(slider.name, {align = 'left'}, suit.layout:row(300, 0))
+    suit.Slider(slider, suit.layout:row(300, 30))
+  end
+
+  mushroom:update(dt)
+end
+
+function love.draw()
+  mushroom:draw()
+  suit.draw()
+end
