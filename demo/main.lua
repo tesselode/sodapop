49818283c508485cf756f6c6239c4faa3afeb6ce
Andrew Minnich <aminnich3@gmail.com>
2016-02-20 17:17:22 -0500

    Functional sliders

--- demo/main.lua
+++ demo/main.lua
@@ -21,11 +21,15 @@ function love.update(dt)
   suit.layout:reset(100, 100)
   suit.layout:padding(10, 10)
 
+  -- sliders
   for _, slider in pairs(sliders) do
     suit.Label(slider.name, {align = 'left'}, suit.layout:row(300, 0))
     suit.Slider(slider, suit.layout:row(300, 30))
   end
 
+  mushroom.r = sliders.rotation.value
+  mushroom.sx = sliders.sx.value
+  mushroom.sy = sliders.sy.value
   mushroom:update(dt)
 end
 
