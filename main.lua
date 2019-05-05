e569da27bc789dd5ed9cfdf5672859946ddf51fd
Andrew Minnich <aminnich3@gmail.com>
2019-05-05 01:43:30 -0400

    basic animation

--- main.lua
+++ main.lua
@@ -1,13 +1,25 @@
 local sodapop = require 'sodapop'
 
-local sheet = sodapop.newSheet('mushroom.png', 16, 16)
+local mushroomSpriteSheet = sodapop.newSheet('mushroom.png', 16, 16)
+local sprite = sodapop.newSprite {
+	sheet = mushroomSpriteSheet,
+	animations = {
+		walk = {
+			frames = {2, 3, 4, 5},
+			durations = {.1, .1, .1, .1},
+		},
+	},
+	startingAnimation = 'walk',
+}
+
+function love.update(dt)
+	sprite:update(dt)
+end
 
 function love.keypressed(key)
 	if key == 'escape' then love.event.quit() end
 end
 
 function love.draw()
-	for i, quad in ipairs(sheet.quads) do
-		love.graphics.draw(sheet.image, quad, i * 24, 0)
-	end
+	sprite:draw(50, 50)
 end
--- sodapop.lua
+++ sodapop.lua
@@ -22,4 +22,44 @@ function sodapop.newSheet(image, frameWidth, frameHeight)
 	}
 end
 
+local Sprite = {}
+Sprite.__index = Sprite
+
+function Sprite:update(dt)
+	local animation = self.animations[self.currentAnimation]
+	animation.timer = animation.timer - dt
+	while animation.timer <= 0 do
+		animation.currentFrame = animation.currentFrame + 1
+		if animation.currentFrame > #animation.frames then
+			animation.currentFrame = 1
+		end
+		animation.timer = animation.timer + animation.durations[animation.currentFrame]
+	end
+end
+
+function Sprite:draw(x, y)
+	local animation = self.animations[self.currentAnimation]
+	local frame = animation.frames[animation.currentFrame]
+	local quad = self.sheet.quads[frame]
+	love.graphics.draw(self.sheet.image, quad, x, y)
+end
+
+function sodapop.newSprite(options)
+	if not options then error('must provide an options table', 2) end
+	local animations = {}
+	for animationName, animation in pairs(options.animations) do
+		animations[animationName] = {
+			frames = animation.frames,
+			durations = animation.durations,
+			currentFrame = 1,
+			timer = animation.durations[1],
+		}
+	end
+	return setmetatable({
+		sheet = options.sheet,
+		animations = animations,
+		currentAnimation = options.startingAnimation,
+	}, Sprite)
+end
+
 return sodapop
